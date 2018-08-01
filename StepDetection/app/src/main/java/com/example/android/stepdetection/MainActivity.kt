94f937880f3d03d28442f423a53601af7213abf1
Janie <emerald1901@gmail.com>
2018-08-01 10:55:38 +0700

    High Pass Filter

--- StepDetection/app/src/main/java/com/example/android/stepdetection/MainActivity.kt
+++ StepDetection/app/src/main/java/com/example/android/stepdetection/MainActivity.kt
@@ -13,33 +13,40 @@ import android.widget.Toast
 import com.jjoe64.graphview.series.DataPoint
 import com.jjoe64.graphview.series.LineGraphSeries
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.IOException
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
     // Constants
-    val ABOVE: Int = 1
-    val BELOW: Int = 0
+    private val ABOVE: Int = 1
+    private val BELOW: Int = 0
+    private val MAX_Y: Double = 20.0
+    private val MAX_Y_FILTER: Double = 10.0
+    private val MIN_X: Double = 4.0
+    private val MAX_X: Double = 50.0
 
     // Variables
-    lateinit var sensorManager: SensorManager
+    private lateinit var sensorManager: SensorManager
 
-    lateinit var rawData: LineGraphSeries<DataPoint>
-    lateinit var lpData: LineGraphSeries<DataPoint>
+    private lateinit var rawData: LineGraphSeries<DataPoint>
+    private lateinit var hpPlot: LineGraphSeries<DataPoint>
+    private lateinit var lpPlot: LineGraphSeries<DataPoint>
 
-    var streakPrevTime: Long = 0
-    var streakStartTime: Long = 0
-    var startTime: Long = 0
+    private var streakPrevTime: Long = 0
+    private var streakStartTime: Long = 0
+    private var startTime: Long = 0
 
-    var prev: FloatArray = FloatArray(3)
+    private var prev: FloatArray = FloatArray(3)
 
-    var stepCount: Int = 0
-    var CURRENT_STATE: Int = 0
-    var PREVIOUS_STATE: Int = BELOW
+    private var stepCount: Int = 0
+    private var CURRENT_STATE: Int = 0
+    private var PREVIOUS_STATE: Int = BELOW
 
-    var rawPoints: Double = 0.0
-    var sampleCount: Int = 0
-    var SAMPLING_ACTIVE: Boolean = true
+    private var rawPoints: Double = 0.0
+    private var sampleCount: Int = 0
+    private var SAMPLING_ACTIVE: Boolean = true
+
+    // High pass filter
+    private var accHPavg: FloatArray = FloatArray(3)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,41 +61,44 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         rawData.color = Color.RED
 
         gv_raw.viewport.isYAxisBoundsManual = true
-        gv_raw.viewport.setMinY(-40.0)
-        gv_raw.viewport.setMaxY(30.0)
+        gv_raw.viewport.setMinY(0.0)
+        gv_raw.viewport.setMaxY(MAX_Y)
 
         gv_raw.viewport.isXAxisBoundsManual = true
-        gv_raw.viewport.setMinX(4.0)
-        gv_raw.viewport.setMaxX(80.0)
+        gv_raw.viewport.setMinX(MIN_X)
+        gv_raw.viewport.setMaxX(MAX_X)
 
         gv_raw.addSeries(rawData)
 
+        // High Pass Filter graph
+        hpPlot = LineGraphSeries()
+        hpPlot.title = "Smooth Data"
+        hpPlot.color = Color.BLUE
+
+        gv_highPass.viewport.isYAxisBoundsManual = true
+        gv_highPass.viewport.setMinY(0.0)
+        gv_highPass.viewport.setMaxY(MAX_Y_FILTER)
+
+        gv_highPass.viewport.isXAxisBoundsManual = true
+        gv_highPass.viewport.setMinX(MIN_X)
+        gv_highPass.viewport.setMaxX(MAX_X)
+
+        gv_highPass.addSeries(hpPlot)
+
         // Low Pass Filter graph
-        lpData = LineGraphSeries()
-        lpData.title = "Smooth Data"
-        lpData.color = Color.BLUE
+        lpPlot = LineGraphSeries()
+        lpPlot.title = "Smooth Data"
+        lpPlot.color = Color.BLUE
 
         gv_lowPass.viewport.isYAxisBoundsManual = true
-        gv_lowPass.viewport.setMinY(-30.0)
-        gv_lowPass.viewport.setMaxY(30.0)
+        gv_lowPass.viewport.setMinY(0.0)
+        gv_lowPass.viewport.setMaxY(MAX_Y_FILTER)
 
         gv_lowPass.viewport.isXAxisBoundsManual = true
-        gv_lowPass.viewport.setMinX(4.0)
-        gv_lowPass.viewport.setMaxX(80.0)
+        gv_lowPass.viewport.setMinX(MIN_X)
+        gv_lowPass.viewport.setMaxX(MAX_X)
 
-        gv_lowPass.addSeries(lpData)
-
-        // Combined Plot
-        gv_combined.viewport.isYAxisBoundsManual = true
-        gv_combined.viewport.setMinY(-70.0)
-        gv_combined.viewport.setMaxY(70.0)
-
-        gv_combined.viewport.isXAxisBoundsManual = true
-        gv_combined.viewport.setMinX(4.0)
-        gv_combined.viewport.setMaxX(80.0)
-
-        gv_combined.addSeries(rawData)
-        gv_combined.addSeries(lpData)
+        gv_lowPass.addSeries(lpPlot)
 
         streakPrevTime = System.currentTimeMillis() - 500
     }
@@ -96,22 +106,26 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     override fun onResume() {
         sensorManager.registerListener(this,
                 sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-                SensorManager.SENSOR_DELAY_NORMAL)
+                50000)
 
         super.onResume()
         startTime = System.currentTimeMillis()
     }
 
     private fun handleEvent(event: SensorEvent) {
-        prev = lowPassFilter(event.values, prev)
+        val hpFiltered = highPassFilter(event.values)
+//        prev = lowPassFilter(event.values, prev)
+        prev = lowPassFilter(hpFiltered, prev)
 
         val raw = Accelerometer(event.values)
-        val data = Accelerometer(prev)
+        val hpData = Accelerometer(hpFiltered)
+        val lpData = Accelerometer(prev)
 
-        rawData.appendData(DataPoint(rawPoints++, raw.R), true, 1000)
-        lpData.appendData(DataPoint(rawPoints, data.R), true, 1000)
+        rawData.appendData(DataPoint(rawPoints++, raw.R), true, 100)
+        hpPlot.appendData(DataPoint(rawPoints, hpData.R), true, 100)
+        lpPlot.appendData(DataPoint(rawPoints, lpData.R), true, 100)
 
-        if (data.R > 10.5) {
+        if (lpData.R > 1.0 && lpData.R < 2.0) {
             CURRENT_STATE = ABOVE
 
             if (PREVIOUS_STATE != CURRENT_STATE) {
@@ -136,8 +150,20 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         tv_counter.text = "$stepCount"
     }
 
+    private fun highPassFilter(input: FloatArray): FloatArray {
+        val alpha = 0.8f
+        val accHPfiltered = FloatArray(3)
+
+        for (i in 0 until input.size) {
+            accHPavg[i] = input[i] * (1 - alpha) + accHPavg[i] * alpha
+            accHPfiltered[i] = input[i] - accHPavg[i]
+        }
+
+        return accHPfiltered
+    }
+
     private fun lowPassFilter(input: FloatArray, prev: FloatArray): FloatArray {
-        val alpha = 0.1f
+        val alpha = 0.8f
 
         for (i in 0 until input.size) {
             prev[i] += alpha * (input[i] - prev[i])
@@ -158,7 +184,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
                         val samplingRate: Double = sampleCount / ((now - startTime) / 1000.0)
                         SAMPLING_ACTIVE = false
                         Toast.makeText(applicationContext,
-                                "Sampling rate ${samplingRate}", Toast.LENGTH_LONG).show()
+                                "Sampling rate $samplingRate", Toast.LENGTH_LONG).show()
                     }
                 }
             }
